d8a567480b3c8753b19162b2e27de11543c7ec27
김형익 <kiomnd2@naver.com>
2019-04-18 18:32:36 +0900

    Chapter2. 조건부 빈 설정

--- chapter2/src/main/kotlin/com/microservices/chapter2/Chapter2Application.kt
+++ chapter2/src/main/kotlin/com/microservices/chapter2/Chapter2Application.kt
@@ -2,7 +2,9 @@ package com.microservices.chapter2
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Controller
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.PathVariable
@@ -15,15 +17,23 @@ class Chapter2Application
 
 @Controller
 class FirstController {
-    @Autowired
-    lateinit var service : ServiceInterface
+
+    @Bean
+    @ConditionalOnExpression("#{'\${service.message.type'=='simple' }")
+    fun exampleService() : ServiceInterface =ExampleService()
+
+    @Bean
+    @ConditionalOnExpression("#{'\${service.message.type'=='advance' }")
+    fun advanceService() : ServiceInterface = AdvanceService()
+
+
 
     @Autowired
     lateinit var service2 : ExampleService2
 
     @RequestMapping(value="/user/{name}", method = arrayOf(RequestMethod.GET))
     @ResponseBody
-    fun hello(@PathVariable name : String) = service.getHello(name)+ '\n' + service2.print()
+    fun hello(@PathVariable name : String) = advanceService().getHello(name) +'\n' + service2.print()
 
 
 
